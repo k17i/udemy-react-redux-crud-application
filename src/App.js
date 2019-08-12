61e1281ddfd1527c58af510ae76c7258e1a6a3fb
Katsu Nakanishi <katsuyoshi.nakanishi@gmail.com>
2019-08-12 09:20:07 +0900

    Learned props

--- src/App.js
+++ src/App.js
@@ -13,17 +13,27 @@ import React, { Component } from 'react';
 //}
 
 const App = () => {
+  const profiles=[
+    {name: "Taro", age: 10},
+    {name: "Hanakko", age: 5},
+    {name: "Noname"}    
+  ]
   return (
   <div>
-    <Cat/>
-    <Cat/>
-    <Cat/>
+    {
+      profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />
+      })
+    }
   </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}. {props.age} years old.</div>
 }
 
+User.defaultProps = {
+  age: 1
+}
 export default App;
